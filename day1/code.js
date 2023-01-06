0fd64fa4d5e0f940e17d86d245249d473c85906e
jiedevm <dieudonne.allognon@yahoo.com>
2023-01-07 00:29:47 +0100

    feat: challenge-1-2

--- day1/code.js
+++ day1/code.js
@@ -1,16 +1,25 @@
-
 const fs = require('fs');
 const readline = require('readline');
 const path = require('path');
 
-var calories = [];
+const calories = [];
 
-readline.createInterface({
-    input: fs.createReadStream(path.resolve(__dirname, './puzzle-input.txt'))
-}).on('line', (line) => {
-    line.trim().length == 0
-    ? calories.push(0)
-    : calories.push((calories.pop() || 0 ) + parseInt(line, 10));
-}).on('close', () => {
-    console.log(`The most carried calories by an Elf is: ${calories.sort((a, b) => a - b).pop()}`);
-});
+readline
+    .createInterface({
+        input: fs.createReadStream(path.resolve(__dirname, './puzzle-input.txt')),
+    })
+    .on('line', (line) => {
+        line.trim().length == 0
+            ? calories.push(0)
+            : calories.push((calories.pop() || 0) + parseInt(line, 10));
+    })
+    .on('close', () => {
+        const [first, second, third, ...rest] = calories.sort((a, b) => b - a);
+
+        console.log(`The most carried calories by an Elf is: ${first}`);
+        console.log(
+            `The carried calories by the top three Elves is : ${
+                first + second + third
+            }`
+        );
+    });
