f8cdf85408483c21689631ce5d45ce1861902136
jiedevm <dieudonne.allognon@yahoo.com>
2023-01-07 20:58:35 +0100

    feat: challenge-4-2

--- day4/code.js
+++ day4/code.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 
 let totalIncludedPairs = 0;
+let totalOverlappedPairs = 0;
 
 readline
     .createInterface({
@@ -14,14 +15,20 @@ readline
         const [range1, range2] = assignment.split(',')
             .map(range => range.split('-').map(section => parseInt(section)));
 
-
-        totalIncludedPairs += (isRangeIncluded(range1, range2) || isRangeIncluded(range2, range1));
+        if (isRangeIncluded(range1, range2) || isRangeIncluded(range2, range1)) {
+            totalIncludedPairs ++;
+            totalOverlappedPairs++;
+        } else {
+            totalOverlappedPairs += (isRangeOverlapped(range1, range2)|| isRangeOverlapped(range2, range1)) ? 1 : 0;
+        }
 
     })
     .on('close', () => {
         console.log(`The total assignments with one range fully containing the other is ${totalIncludedPairs}`);
+        console.log(`The total assignments with one range overlapping the other is ${totalOverlappedPairs}`);
     });
 
 
 
 const isRangeIncluded = (haystack, needle) => needle[0] >= haystack[0] && needle[1] <= haystack[1];
+const isRangeOverlapped = (haystack, needle) => needle.find((section) => section >= haystack[0] && section <= haystack[1]);
