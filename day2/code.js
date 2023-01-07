050104e8ebb5c6f75cd8acaf6b1a1229cc01cbe4
jiedevm <dieudonne.allognon@yahoo.com>
2023-01-07 13:35:38 +0100

    feat: challenge2-2

--- day2/code.js
+++ day2/code.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 const readline = require('readline');
 const path = require('path');
 
-let totalScore = 0;
-
 
 const [opponent, challenger] = [['A', 'B', 'C'], ['X', 'Y', 'Z']];
 
@@ -11,12 +9,9 @@ const choicesSeparator = ' ';
 
 const charCodesDiff = challenger[0].charCodeAt(0) - opponent[0].charCodeAt(0);
 
-const clashPoints = (opponentChoice, choice) =>
-    [ 1, 2, 0].at(
-        choice.charCodeAt(0) - (opponentChoice.charCodeAt(0) + charCodesDiff)
-    )*3;
 
-const choicePoints = (choice) => choice.charCodeAt(0) - challenger[0].charCodeAt(0) + 1;
+let totalScore1 = 0;
+let totalScore2 = 0;
 
 readline
     .createInterface({
@@ -25,8 +20,31 @@ readline
     .on('line', (line) => {
         let [a, b] = line.trim().split(choicesSeparator);
 
+        const newChoice = predictedChoice(a, b);
+
         if (a && b) {
-            totalScore += clashPoints(a, b) + choicePoints(b);
+            totalScore1 += clashPoints(a, b) + choicePoints(b);
+            totalScore2 += clashPoints(a, newChoice) + choicePoints(newChoice);
         }
     })
-    .on('close', () => console.log(`The 1st strategy score is ${totalScore}`));
+    .on('close', () => {
+        console.log(`The 1st strategy score is ${totalScore1}`);
+        console.log(`The 2nd strategy score is ${totalScore2}`);
+    });
+
+
+
+const clashPoints = (opponentChoice, choice) =>
+    [ 1, 2, 0].at(
+        choice.charCodeAt(0) - (opponentChoice.charCodeAt(0) + charCodesDiff)
+    )*3;
+
+const choicePoints = (choice) => choice.charCodeAt(0) - challenger[0].charCodeAt(0) + 1;
+
+const predictedChoice = (opponentChoice, choice) => {
+
+    const move = [-1, 0, 1].at(challenger.indexOf(choice));
+    const position = opponent.indexOf(opponentChoice);
+
+    return challenger.at((position + move)%challenger.length);
+};
