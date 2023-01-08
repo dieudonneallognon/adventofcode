c6757e03920978417a673822af21496b200212ed
jiedevm <dieudonne.allognon@yahoo.com>
2023-01-08 20:02:53 +0100

    feat: challenge-6-2

--- day6/code.js
+++ day6/code.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 
-const [a, b, c, d, ...rest] = fs.readFileSync(
+const data = fs.readFileSync(
     path.resolve(__dirname, './puzzle-input.txt'),
     { encoding: 'utf-8', flag: 'r' }
 ).split('');
 
-
+let [a, b, c, d, ...rest] = data;
 const packetStartChars = [a, b, c, d];
 const dummyChars = [];
 
@@ -20,4 +20,20 @@ console.log(
     'The number of characters to be processed before the first ' +
     'start-of-message marker is detected is '+
     (packetStartChars.length + dummyChars.length)
+);
+
+const msgChars = data.slice(0, 14);
+rest = data.slice(14);
+
+dummyChars.length = 0;
+
+while (!msgChars.every((c, i, self) => (self.filter(e => e === c).length === 1))) {
+    dummyChars.push(msgChars.shift());
+    msgChars.push(rest.shift());
+}
+
+console.log(
+    'The number of characters to be processed before the first ' +
+    'start-of-message marker is detected is '+
+    (msgChars.length + dummyChars.length)
 );
