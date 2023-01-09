f0b4b5433df07196cf909446e88e6fa8aae4627f
jiedevm <dieudonne.allognon@yahoo.com>
2023-01-09 10:37:13 +0100

    feat: challenge-7-2

--- day7/code.js
+++ day7/code.js
@@ -6,6 +6,9 @@ const directories = [];
 
 let currentDir = null;
 
+const UPDATE_SPACE = 30000000;
+const DISK_SPACE = 70000000;
+
 readline
     .createInterface({
         input: fs.createReadStream(path.resolve(__dirname, './puzzle-input.txt')),
@@ -57,13 +60,23 @@ readline
         }
     })
     .on('close', () => {
-        dirSize('/', 0);
+        const usedSpace = dirSize('/', 0);
+        const freeSpace = (DISK_SPACE - usedSpace);
 
         console.log(
             'The directories with a total size of at most 100000 total sizes sum is ' +
-            directories
+            JSON.parse(JSON.stringify(directories))
                 .filter((dir) => (dir.size <= 100000))
                 .reduce((sum, dir) => (sum += dir.size), 0)
+        );
+
+        console.log(
+            `The directories with the smallest size to delete to free ${UPDATE_SPACE} is ` +
+            JSON.parse(JSON.stringify(directories))
+                .reduce((max, dir) => (
+                    freeSpace + dir.size >= UPDATE_SPACE &&
+                    dir.size < max
+                ) ? dir.size : max, usedSpace)
         );
     });
 
